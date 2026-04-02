fb887257da53cced3162207707dd275a51407d2f
ffushun <147054362+ffushun@users.noreply.github.com>
2026-04-02 16:19:23 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,5 @@
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config.js";
 
-console.log("SUPABASE_URL=", SUPABASE_URL);
-console.log("SUPABASE_ANON_KEY=", SUPABASE_ANON_KEY);
-
 export const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
